1cdde8700168abc35fddfd07136eff8bff1a21f9
Alexandru Radu-Todor <50906589+AlexandruRT631@users.noreply.github.com>
2021-12-07 12:21:19 +0200

    day 7 first puzzle solution

---
+++ src/adventofcode2021/D7P1.java
@@ -0,0 +1,23 @@
+package adventofcode2021;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+import static java.lang.Math.abs;
+
+public class D7P1 implements PuzzleBasics {
+    @Override
+    public void run() throws IOException {
+        Reader reader = new Reader();
+        String line = reader.readFromInput("resources/day7.txt").get(0);
+        int[] positions = Arrays.stream(line.split(",")).mapToInt(Integer::parseInt).toArray();
+        Arrays.sort(positions);
+        int median = positions[positions.length / 2];
+        median = positions.length % 2 == 1? (median + positions[positions.length / 2 + 1]) / 2 : median;
+        int fuel = 0;
+        for (int position : positions) {
+            fuel = fuel + abs(position - median);
+        }
+        System.out.println(fuel);
+    }
+}
--- src/adventofcode2021/Main.java
+++ src/adventofcode2021/Main.java
@@ -4,6 +4,6 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] argv) throws IOException {
-        new D6P2().run();
+        new D7P1().run();
     }
 }
